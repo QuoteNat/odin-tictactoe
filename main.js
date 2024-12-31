a0873408737f221e2b2f811d9d1c8b100df803da
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2024-12-31 14:04:39 -0600

    Add createPlayer function

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+// TODO: Rename this to something more logical?
 const SquareState = {
     EMPTY: 0,
     O: 1,
     X: 2
-}
+};
+
+// squareType refers to the square the player places (X or O)
+function createPlayer(squareType) {
+    let type = squareType;
+    let score = 0;
+    return {type, score};
+};
 
 function createGame() {
     let board = [
@@ -12,22 +20,20 @@ function createGame() {
     ];
     for (let i=0; i < 3; i++) {
         board[i].fill(SquareState.EMPTY);
-    }
+    };
 
     console.log(board);
     let currentPlayerState = SquareState.X;
-    const doTurn = (x, y) => {
+    const doTurn = (player, x, y) => {
         if (y >= 0 && y < 3 && x >= 0 && x <= 3 && board[y][x] === SquareState.EMPTY) {
-            board[y][x] = currentPlayerState;
-            if (currentPlayerState === SquareState.X) {
-                currentPlayerState = SquareState.O;
-            } else {
-                currentPlayerState = SquareState.X;
-            }
+            board[y][x] = player.type;
         }
-        console.log(board)
+        console.log(board);
     }
-    return {doTurn}
+    return {doTurn};
 }
 
+let playerX = createPlayer(SquareState.X);
+let playerO = createPlayer(SquareState.O);
+let players = [playerX, playerO];
 let game = createGame();
