7cf6630caa2d472171a2ed8d5a905254d17fa1b0
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2024-12-31 14:43:44 -0600

    Add victory checking

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const SquareState = {
     X: 2
 };
 
+const VictoryState = {
+    WIN: 0,
+    TIE: 1,
+    NONE: 2,
+}
+
 // squareType refers to the square the player places (X or O)
 function createPlayer(squareType) {
     let type = squareType;
@@ -18,15 +24,56 @@ function createGame() {
         new Array(3), 
         new Array(3)
     ];
+
+    let turnsDone = 0;
+
     for (let i=0; i < 3; i++) {
         board[i].fill(SquareState.EMPTY);
-    };
+    }
+
+    const checkForWin = () => {
+        // Tie case: All tiles filled with no win condition met
+        if (turnsDone >= 9) {
+            return VictoryState.TIE;
+        }
+
+        // Win case 1: Same values in a row
+        for (row in board) {
+            console.log(row);
+            if (board[row][0] != SquareState.EMPTY && board[row][0] == board[row][1] && board[row][0] == board[row][2]) {
+                console.log ("Win via case 1")
+                return VictoryState.WIN;
+            }
+        }
+
+        // Win case 2: Diagonals
+        if (board[0][0] !== SquareState.EMPTY && board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+            return VictoryState.WIN;
+        }        
+        if (board[0][2] !== SquareState.EMPTY && board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+            return VictoryState.WIN;
+        }
+
+        // Win case 3: Columns
+        for (let i=0; i<3; i++) {
+            if (board[0][i] !== SquareState.EMPTY && board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+                return VictoryState.WIN;
+            }
+        }
+    }
 
     console.log(board);
     let currentPlayerState = SquareState.X;
     const doTurn = (player, x, y) => {
         if (y >= 0 && y < 3 && x >= 0 && x <= 3 && board[y][x] === SquareState.EMPTY) {
             board[y][x] = player.type;
+            turnsDone++;
+            let victory = checkForWin();
+            if (victory === VictoryState.WIN) {
+                console.log("WIN");
+            } else if (victory === VictoryState.TIE) {
+                console.log("TIE")
+            }
         }
         console.log(board);
     }
