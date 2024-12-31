b4d805824f13c2c1b3bf67122f17ecb913330ed1
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2024-12-31 13:52:59 -0600

    Add doTurn method

--- main.js
+++ main.js
@@ -11,16 +11,23 @@ function createGame() {
         new Array(3)
     ];
     for (let i=0; i < 3; i++) {
-        board[i].fill(SquareState.EMPTY)
+        board[i].fill(SquareState.EMPTY);
     }
 
     console.log(board);
-    let currentPlayer = SquareState.X;
+    let currentPlayerState = SquareState.X;
     const doTurn = (x, y) => {
-        if (y >= 0 && y < 3) {
-
+        if (y >= 0 && y < 3 && x >= 0 && x <= 3 && board[y][x] === SquareState.EMPTY) {
+            board[y][x] = currentPlayerState;
+            if (currentPlayerState === SquareState.X) {
+                currentPlayerState = SquareState.O;
+            } else {
+                currentPlayerState = SquareState.X;
+            }
         }
+        console.log(board)
     }
+    return {doTurn}
 }
 
 let game = createGame();
